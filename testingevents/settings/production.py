0fc97abd53c4d6887b41796d381aa8847a44ad6d
Oleg <slepcov_oleg@mail.ru>
2023-03-20 17:58:09 +0700

    First commit

--- testingevents/settings/production.py
+++ testingevents/settings/production.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -112,3 +115,6 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 100
 }
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
