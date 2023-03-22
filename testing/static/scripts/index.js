e77290185961e3aa2b317e8dab91ba1710e04c12
Oleg <slepcov_oleg@mail.ru>
2023-03-22 17:52:24 +0700

    Added editor events

--- testing/static/scripts/index.js
+++ testing/static/scripts/index.js
@@ -1,4 +1,4 @@
-import App from './script.js';                                                                     // модуль выполнения работы к Битрикс
+import App from './change_events.js';                                                                     // модуль выполнения работы к Битрикс
 
 document.addEventListener("DOMContentLoaded", () => {
     BX24.ready(function() {
